089f379e0b68a3171819d25f0902037577972f60
sapanapoudel <sapana.poudel17@outlook.com>
2019-06-06 10:56:02 -0700

    fixed no. 6

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 //Declare variable and use prompts
 
 //Receiving and using username
+
 var username = prompt('What is your username');
 var message = 'My username is: ' + username;
 alert(message);
@@ -10,6 +11,7 @@ console.log('username: ' + username);
 
 var correctAnswer = 0;
 //Fungame 1
+
 var favoriteFood = prompt('Is my favorite food spicy');
 console.log('favoriteFood: ' +favoriteFood.toLocaleUpperCase()); 
 
@@ -82,26 +84,31 @@ var randomNumber = Math.ceil(Math.random()*50) +1;
 console.log('randomNumber: ' + randomNumber);
 
 var guessState = parseInt(prompt('Guess how many state I have visited so far'));
-console.log('guessState: ' + guessState);
+console.log('guessState: ' + guessState + ' is a ' + typeof guessState);
 
-//label to loop1
-loop1: 
+
 //It will enter loop 4 times
-for(var i = 0; i < 4; i++){
+for(var i = 0; i < 3; i++){
+  console.log('guessState: ' + guessState + ' is a ' + typeof guessState);
+
   if (guessState === randomNumber){
-    alert('Hey congratulation! you got it');
-    correctAnswer++;
-    break loop1;
+    break;
 
   }else if(guessState > randomNumber){
     guessState = prompt('Your number is too high, guess again');
 
   }else if(guessState < randomNumber){
     guessState = prompt('Your number is too low, guess again');
-
+  
   }else{
     guessState = prompt('Please enter number');
   }
+  guessState = parseInt(guessState);
+}
+
+if (guessState === randomNumber){
+  alert('Hey congratulation! you got it');
+  correctAnswer++;
 }
 
 //==================================================================================================================
@@ -117,7 +124,7 @@ console.log('customerChoice: ' + customerChoice);
 var attempt = 1;  
 
 //label while loop to loop2
-loop2:
+loop2: //source: https://stackoverflow.com/questions/183161/whats-the-best-way-to-break-from-nested-loops-in-javascript
 //it will loop through 6 attampts
 while (attempt < 6){
   //loop through each element in Array
